11f0da07363d224877a2e979c57ab9a0a6499035
Andrew <andrewly07@gmail.com>
2019-09-23 04:49:31 +0000

    completed problem 16

---
+++ practice16/main.js
@@ -0,0 +1,13 @@
+function danielp_spitSequence(N){
+    var array=[];
+    for(var i=N; i<(N+11);i++){
+      if(!(i%3)){
+        array.push(i);
+      }
+      else
+      {
+        array.push(0);    
+      }
+    }
+    return array;
+  }
